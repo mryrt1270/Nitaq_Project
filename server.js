534a160d1573a459e6a27314353e20b635b8fcab
Extee505 <turki.mr.2000@gmail.com>
2026-04-05 05:23:35 +0300

    fix session crash FINAL

--- server.js
+++ server.js
@@ -64,7 +64,9 @@ if (!users.find(u => u.email === "owner@nitaq.sa")) {
 // ================== HELPERS ==================
 
 function requireLogin(req, res, next) {
-    if (!req.session.user) return res.redirect('/login');
+    if (!req.session || !req.session.user) {
+        return res.redirect('/login');
+    }
     next();
 }
 
